6a6bc7c19715af23b005f538861fe5da625a4275
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2021-06-28 01:18:54 +0300

    added main component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  isSideBarOpen = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sideBarToggler() {
+    this.isSideBarOpen = !this.isSideBarOpen;
+  }
+
+}
