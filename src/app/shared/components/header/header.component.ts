06a2e52c0b3faaf9d1220311be33e998bf7d47e9
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2021-06-28 01:17:07 +0300

    added header component

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
