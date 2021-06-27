bd368ccfc4927c84a7b93f168476b992ae4cf6a4
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2021-06-27 14:07:17 +0300

    added functionality

--- src/app/shared/log/log.service.ts
+++ src/app/shared/log/log.service.ts
@@ -1,13 +1,49 @@
 import { Injectable } from '@angular/core';
 
+export enum LogLevel {
+  All = 0,
+  Debug = 1,
+  Info = 2,
+  Warn = 3,
+  Error = 4,
+  Fatal = 5,
+  Off = 6
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
+  level: LogLevel = LogLevel.All;
+  logWithDate: boolean = true;
 
   constructor() { }
 
+  private shouldLog(level: LogLevel): boolean {
+    if (level !== LogLevel.Off && level >= this.level) {
+      return true;
+    }
+
+    return false;
+  }
+
+  writeToLog(msg: string, level: LogLevel) {
+    if (this.shouldLog(level)) {
+
+      let value: string = "";
+      if (this.logWithDate) {
+        value = new Date() + ' - ';
+      }
+
+      value += "Type: " + LogLevel[level];
+      value += " - Message: " + JSON.stringify(msg);
+
+      // Log the value
+      console.log(value);
+    }
+  }
+
   log(msg: any) {
-    console.log(new Date() + ': ' + JSON.stringify(msg));
+    this.writeToLog(msg, LogLevel.All);
   }
 }
