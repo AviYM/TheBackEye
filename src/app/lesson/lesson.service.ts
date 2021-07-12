129c0a7cdccb2b9381a346077d4cd7c4b3ff7ab4
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2021-07-12 16:57:00 +0300

    some changes

--- src/app/lesson/lesson.service.ts
+++ src/app/lesson/lesson.service.ts
@@ -15,4 +15,24 @@ export class LessonService {
     this.logger.log("The LessonService.getLessons() is called");
     return this.http.read<ILesson[]>('MOCK_DATA_lessons.json');
   }
+
+  public getSingleLesson(id: number): Observable<ILesson> {
+    this.logger.log("The LessonService.getSingleLesson() is called");
+    return this.http.read<ILesson>('MOCK_DATA_lessons.json/' + id);
+  }
+
+  public addLesson(newLesson: ILesson): Observable<ILesson> {
+    this.logger.log("The LessonService.addLesson() is called");
+    return this.http.create<ILesson>('MOCK_DATA_lessons.json', newLesson);
+  }
+
+  public editLesson(lesson: ILesson): Observable<ILesson> {
+    this.logger.log("The LessonService.editLesson() is called");
+    return this.http.update<ILesson>('MOCK_DATA_lessons.json', lesson);
+  }
+
+  public removeLesson(id: number): Observable<ILesson> {
+    this.logger.log("The LessonService.removeLesson() is called");
+    return this.http.delete<ILesson>('MOCK_DATA_lessons.json', id);
+  }
 }
--- src/app/shared/services/http/http.service.ts
+++ src/app/shared/services/http/http.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, map, tap } from "rxjs/operators"
+import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { isNull } from 'util';
 import { LogService } from '../log/log.service';
 
 interface QueryParams {
@@ -11,45 +10,49 @@ interface QueryParams {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
   private readonly END_POINT: string;
 
-  constructor(private http: HttpClient, private logger: LogService) { 
+  constructor(private http: HttpClient, private logger: LogService) {
     this.END_POINT = environment.api.baseUrl; //`${environment.api.baseUrl}`
   }
 
   public create<T>(route: string, item: T): Observable<T> {
-    return this.http
-      .post<T>(`${this.END_POINT}/${route}`, item)
-      .pipe();
+    return this.http.post<T>(`${this.END_POINT}/${route}`, item).pipe(
+      tap((data) => this.logger.log('createItem: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
 
   public read<T>(route: string): Observable<T> {
-    this.logger.log("HttpService.getItems is called");
+    this.logger.log('HttpService.getItems is called');
     // const cfqu = this.correctFormatForQueryUrl(qp);
     return this.http.get<T>(`${this.END_POINT}/${route}`).pipe(
-      tap(data => this.logger.log(JSON.stringify(data))),
+      tap((data) => this.logger.log('getItem: ' + JSON.stringify(data))),
       catchError(this.handleError)
     );
   }
 
   public update<T>(route: string, item: T): Observable<T> {
-    return this.http
-      .put<T>(`${this.END_POINT}/${route}`, item)
-      .pipe();
+    return this.http.put<T>(`${this.END_POINT}/${route}`, item).pipe(
+      tap((data) => this.logger.log('updateItem: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
 
   public delete<T>(route: string, id: number): Observable<T> {
-    return this.http
-      .delete<T>(`${this.END_POINT}/${route}/${id}`);
+    return this.http.delete<T>(`${this.END_POINT}/${route}/${id}`).pipe(
+      tap((data) => this.logger.log('deleteItem: ' + id)),
+      catchError(this.handleError)
+    );
   }
 
   private handleError(err: HttpErrorResponse) {
     let errorMessage = '';
 
-    if(err.error instanceof ErrorEvent) {
+    if (err.error instanceof ErrorEvent) {
       errorMessage = `An error accurred: ${err.error.message}`;
     } else {
       errorMessage = `Server returned code: ${err.status}, error nessage is: ${err.message}`;
@@ -59,7 +62,7 @@ export class HttpService {
   }
 
   private correctFormatForQueryUrl(qp: QueryParams): string {
-    if (isNull(qp)) {
+    if (qp == null) {
       return '';
     }
     const qpAsStr = this.mapQueryParamsToUrl(qp);
@@ -71,5 +74,4 @@ export class HttpService {
       return `${key}=${qp[key]}`;
     });
   }
-
 }
