613e29669a8ef34c1474ccf14172987d5150c59e
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2021-08-18 22:01:02 +0300

    added signal-R files

--- src/app/teacher/sign-in/sign-in.component.ts
+++ src/app/teacher/sign-in/sign-in.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { currentTeacherChangedAction, TeacherAuthService } from '../teacher-auth.service';
+import { TeacherAuthService } from '../teacher-auth.service';
 
 @Component({
   selector: 'app-sign-in',
