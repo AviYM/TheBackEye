69fbf590c180dfc131ff898a68084e5d27a2508f
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2021-08-19 12:15:57 +0300

    signalR init

--- src/app/dashboard/signal-r.service.ts
+++ src/app/dashboard/signal-r.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { IMeasurement } from './measurement.interface';
+import * as signalR from '@microsoft/signalr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignalRService {
+  public emitMeasurements: BehaviorSubject<IMeasurement[]>;
 
-  constructor() { }
+  private remote =
+    'https://datamanagementapi20210728183549.azurewebsites.net/api/Measurement/TestSignalR';
+
+  private connection: signalR.HubConnection;
+
+  public startConnection = () => {
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl(this.remote) // { skipNegotiation: true, transport: signalR.HttpTransportType.WebSockets }
+      .build();
+
+    this.connection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch((err) => console.log('Error while starting connection: ' + err));
+  };
+
+  public addTransferChartDataListener = () => {
+    this.connection.on('TransferMeasurements', (data) => {
+      this.emitMeasurements.next(data);
+      console.log(data);
+    });
+  }
+
+  constructor() {
+    this.emitMeasurements = new BehaviorSubject<IMeasurement[]>(null);
+  }
 }
